89e00815152b00bbd1d331111bdcacc17ec943a1
Zoltan Feher <lostface@fastmail.fm>
2016-09-23 17:23:31 +0200

    Remove some commented config

--- webpack.config.js
+++ webpack.config.js
@@ -32,11 +32,7 @@ module.exports = {
     loaders: [
       {
         test: /\.js$/,
-        // exclude: /(node_modules|bower_components)/,
         loader: 'babel',
-        // query: {
-        //   presets: ['es2015']
-        // }
       }
     ],
 
