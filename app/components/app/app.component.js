a4416ff5a0a332d04ce8b4f19ff4f8347f9b51e9
Zoltan Feher <lostface@fastmail.fm>
2016-09-24 16:33:27 +0200

    Push down repo click handling to content from app

--- app/components/app/app.component.js
+++ app/components/app/app.component.js
@@ -8,21 +8,6 @@ class AppController {
       { id: '456', name: 'repo 2' },
       { id: '789', name: 'repo 3' },
     ];
-
-    // TODO temporary data
-    this.selectedRepo = null;
-
-    // TODO temporary data
-    this.selectedRepoIssues = [
-      { id: '123', title: 'title 1' },
-      { id: '456', title: 'title 2' },
-      { id: '789', title: 'title 3' },
-    ];
-  }
-
-  handleOnRepositoryClick(repositoryId) {
-    // TODO
-
   }
 }
 
--- app/components/content/content.component.js
+++ app/components/content/content.component.js
@@ -1,11 +1,28 @@
 import template from './content.component.html';
 
+class ContentController {
+  $onInit() {
+    // TODO temporary data
+    this.selectedRepo = null;
+
+    // TODO temporary data
+    this.selectedRepoIssues = [
+      { id: '123', title: 'title 1' },
+      { id: '456', title: 'title 2' },
+      { id: '789', title: 'title 3' },
+    ];
+  }
+
+  handleOnRepositoryClick(repositoryId) {
+    // TODO
+    console.log('heyo', repositoryId)
+  }
+}
+
 export default {
   template,
+  controller: ContentController,
   bindings: {
     repositories: '<',
-    selectedRepoFullName: '@',
-    selectedRepoIssues: '<',
-    onRepositoryClick: '&',
   },
 };
