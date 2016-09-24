a8c102b39af9220ebec6927d49447c6a0465475f
Zoltan Feher <lostface@fastmail.fm>
2016-09-24 14:30:41 +0200

    Move test data to app component

--- app/components/app/app.component.js
+++ app/components/app/app.component.js
@@ -2,8 +2,19 @@ import template from './app.component.html';
 
 class AppController {
   $onInit() {
-    this.repositories = [];
-    this.issues = [];
+    // TODO temporary data
+    this.repositories = [
+      { id: '123', name: 'repo 1' },
+      { id: '456', name: 'repo 2' },
+      { id: '789', name: 'repo 3' },
+    ];
+
+    // TODO temporary data
+    this.issues = [
+      { id: '123', title: 'title 1' },
+      { id: '456', title: 'title 2' },
+      { id: '789', title: 'title 3' },
+    ];
   }
 }
 
--- app/components/repository-list/repository-list.component.js
+++ app/components/repository-list/repository-list.component.js
@@ -1,14 +1,6 @@
 import template from './repository-list.component.html';
 
 class RepositoryListController {
-  $onInit() {
-    // TODO remove temporary data
-    this.repositories = [
-      { id: '123', name: 'repo 1' },
-      { id: '456', name: 'repo 2' },
-      { id: '789', name: 'repo 3' },
-    ];
-  }
 }
 
 export default {
