d194753ad3af27dc1125299cd87f696f0beb9ae1
Zoltan Feher <lostface@fastmail.fm>
2016-09-25 17:04:08 +0200

    Extract issues search from handler func

--- app/components/content/content.component.js
+++ app/components/content/content.component.js
@@ -12,14 +12,8 @@ class ContentController {
     this.selectedRepoIssues = [];
   }
 
-  handleOnRepositoryClick(repositoryId) {
-    // TODO more FP-ish
-    const findRepoByRepositoryId = R.find(repository => repository.id === repositoryId);
-    const repo = findRepoByRepositoryId(this.repositories);
-
-    this.selectedRepo = repo;
-
-    this.issueSearchService.searchByRepoFullName(repo.fullName)
+  searchIssuesByRepoFullName(repoFullName) {
+    this.issueSearchService.searchByRepoFullName(repoFullName)
       .then(issues => {
         this.selectedRepoIssues = issues;
       })
@@ -28,6 +22,15 @@ class ContentController {
         console.error(err);
         this.selectedRepoIssues = [];
       });
+  }
+
+  handleOnRepositoryClick(repositoryId) {
+    // TODO more FP-ish
+    const findRepoByRepositoryId = R.find(repository => repository.id === repositoryId);
+    const repo = findRepoByRepositoryId(this.repositories);
+
+    this.selectedRepo = repo;
+    this.searchIssuesByRepoFullName(this.selectedRepo.fullName);
   }
 }
 
