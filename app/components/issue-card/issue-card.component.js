bf9132ba648ccd1af6f80418ba279bd690bd1c5a
Zoltan Feher <lostface@fastmail.fm>
2016-09-25 17:32:35 +0200

    Rationalize binding in issue card component

--- app/components/issue-card/issue-card.component.js
+++ app/components/issue-card/issue-card.component.js
@@ -3,14 +3,6 @@ import template from './issue-card.component.html';
 export default {
   template,
   bindings: {
-    id: '<',
-    number: '<',
-    title: '<',
-    htmlUrl: '<',
-    userLogin: '<',
-    labels: '<',
-    state: '<',
-    comments: '<',
-    createdAt: '<',
+    issue: '<',
   },
 };
