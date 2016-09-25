b09c0705ce172db75c6fa193dbc0b67732e3922d
Zoltan Feher <lostface@fastmail.fm>
2016-09-25 13:57:59 +0200

    Use one way binding to keep types in issue comps

--- app/components/issue-card/issue-card.component.js
+++ app/components/issue-card/issue-card.component.js
@@ -3,14 +3,14 @@ import template from './issue-card.component.html';
 export default {
   template,
   bindings: {
-    id: '@',
-    number: '@',
-    title: '@',
-    htmlUrl: '@',
-    userLogin: '@',
-    labels: '@',
-    state: '@',
-    comments: '@',
-    createdAt: '@',
+    id: '<',
+    number: '<',
+    title: '<',
+    htmlUrl: '<',
+    userLogin: '<',
+    labels: '<',
+    state: '<',
+    comments: '<',
+    createdAt: '<',
   },
 };
--- app/components/issue-list/issue-list.component.js
+++ app/components/issue-list/issue-list.component.js
@@ -3,7 +3,7 @@ import template from './issue-list.component.html';
 export default {
   template,
   bindings: {
-    repoFullName: '@',
+    repoFullName: '<',
     issues: '<',
   },
 };
