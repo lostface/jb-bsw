6c5e9f0015bbe170a6940ab467d86c150bf751a4
Zoltan Feher <lostface@fastmail.fm>
2016-09-27 08:35:55 +0200

    Move back selected props handling from content to app component

--- app/components/app/app.component.js
+++ app/components/app/app.component.js
@@ -1,15 +1,24 @@
+import R from 'ramda';
 import template from './app.component.html';
 
 class AppController {
-  constructor(repositorySearchService) {
+  constructor(repositorySearchService, issueSearchService) {
     'ngInject';
     this.repositorySearchService = repositorySearchService;
+    this.issueSearchService = issueSearchService;
   }
 
   $onInit() {
     this.repositories = [];
+    this.resetSelectedProps();
+
     // initiate an "empty" search
     this.searchRepositories('');
+  }
+
+  resetSelectedProps() {
+    this.selectedRepo = null;
+    this.selectedRepoIssues = [];
   }
 
   searchRepositories(query) {
@@ -24,7 +33,29 @@ class AppController {
         // TODO proper error handling
         console.error(err);
         this.repositories = [];
+      })
+      .finally(() => this.resetSelectedProps());
+  }
+
+  searchIssuesByRepoFullName(repoFullName) {
+    this.issueSearchService.searchByRepoFullName(repoFullName)
+      .then(issues => {
+        this.selectedRepoIssues = issues;
+      })
+      .catch(err => {
+        // TODO proper error handling
+        console.error(err);
+        this.selectedRepoIssues = [];
       });
+  }
+
+  handleOnRepositoryClick(repositoryId) {
+    // TODO more FP-ish
+    const findRepoByRepositoryId = R.find(repository => repository.id === repositoryId);
+    const repo = findRepoByRepositoryId(this.repositories);
+
+    this.selectedRepo = repo;
+    this.searchIssuesByRepoFullName(this.selectedRepo.fullName);
   }
 }
 
--- app/components/content/content.component.js
+++ app/components/content/content.component.js
@@ -1,51 +1,11 @@
-import R from 'ramda';
 import template from './content.component.html';
-
-class ContentController {
-  constructor(issueSearchService, $scope) {
-    'ngInject';
-    this.issueSearchService = issueSearchService;
-
-    $scope.$watch('$ctrl.repositories', () => {
-      this.resetSelectedProps();
-    });
-  }
-
-  $onInit() {
-    this.resetSelectedProps();
-  }
-
-  resetSelectedProps() {
-    this.selectedRepo = null;
-    this.selectedRepoIssues = [];
-  }
-
-  searchIssuesByRepoFullName(repoFullName) {
-    this.issueSearchService.searchByRepoFullName(repoFullName)
-      .then(issues => {
-        this.selectedRepoIssues = issues;
-      })
-      .catch(err => {
-        // TODO proper error handling
-        console.error(err);
-        this.selectedRepoIssues = [];
-      });
-  }
-
-  handleOnRepositoryClick(repositoryId) {
-    // TODO more FP-ish
-    const findRepoByRepositoryId = R.find(repository => repository.id === repositoryId);
-    const repo = findRepoByRepositoryId(this.repositories);
-
-    this.selectedRepo = repo;
-    this.searchIssuesByRepoFullName(this.selectedRepo.fullName);
-  }
-}
 
 export default {
   template,
-  controller: ContentController,
   bindings: {
     repositories: '<',
+    selectedRepo: '<',
+    selectedRepoIssues: '<',
+    onRepositoryClick: '&',
   },
 };
