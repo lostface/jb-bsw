f86c445499787ceb5388a1f1ac65c5d7ef67a882
Zoltan Feher <lostface@fastmail.fm>
2016-09-27 15:01:26 +0200

    Implement issue card component

---
+++ app/components/issue-card/issue-card.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'issue-card',
+  template: `
+    <div id="issue-card-container-{{issue.id}}">
+      <p>Issue Card</p>
+      <p>
+        {{issue.number}}
+        {{issue.title}}
+        {{issue.htmlUrl}}
+        {{issue.userLogin}}
+        {{issue.labels}}
+        {{issue.state}}
+        {{issue.comments}}
+        {{issue.createdAt}}
+      </p>
+    </div>
+  `,
+})
+
+export default class IssueCardComponent {
+  @Input() public issue: any;
+}
