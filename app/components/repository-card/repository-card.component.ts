576eaf8904ce6ce1c9873ed76247ee90e7bcbfdd
Zoltan Feher <lostface@fastmail.fm>
2016-09-27 14:03:33 +0200

    Implement repo card component

---
+++ app/components/repository-card/repository-card.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'repository-card',
+  template: `
+    <div id="repository-card-container-{{repository.id}}" >
+      <p>Repo Card</p>
+      <p>
+        {{repository.name}}
+        {{repository.fullName}}
+        {{repository.htmlUrl}}
+        {{repository.description}}
+        {{repository.stargazersCount}}
+        {{repository.watchersCount}}
+        {{repository.language}}
+        {{repository.forksCount}}
+        {{repository.openIssuesCount}}
+      </p>
+    </div>
+  `,
+})
+
+export default class RepositoryCardComponent {
+  @Input() public repository: any;
+}
