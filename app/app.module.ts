74e915f8e3ffd78863611f97b79113acbe945ae1
Zoltan Feher <lostface@fastmail.fm>
2016-09-27 18:16:07 +0200

    Wire up components in app module

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,13 +1,42 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule }    from '@angular/http';
+import IssueSearchService from './services/issue-search.service';
+import RepositorySearchService from './services/repository-search.service';
 import AppComponent from './components/app/app.component';
+import SearchPanelComponent from './components/search-panel/search-panel.component';
+import ContentComponent from './components/content/content.component';
+import RepositoryListComponent from './components/repository-list/repository-list.component';
+import RepositoryCardComponent from './components/repository-card/repository-card.component';
+import IssueListComponent from './components/issue-list/issue-list.component';
+import IssueCardComponent from './components/issue-card/issue-card.component';
+import './rxjs-extensions';
 
 @NgModule({
-  imports: [BrowserModule],
-  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+  ],
+
+  providers: [
+    RepositorySearchService,
+    IssueSearchService,
+  ],
+
+  declarations: [
+    AppComponent,
+    SearchPanelComponent,
+    ContentComponent,
+    RepositoryListComponent,
+    RepositoryCardComponent,
+    IssueListComponent,
+    IssueCardComponent,
+  ],
+
   bootstrap: [AppComponent],
 })
 
 export default class AppModule {
-
 }
