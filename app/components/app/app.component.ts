200f6c868c6bfcd6d2645857a885db39c91e879f
Zoltan Feher <lostface@fastmail.fm>
2016-09-27 18:12:36 +0200

    Implement app component

--- app/components/app/app.component.ts
+++ app/components/app/app.component.ts
@@ -1,10 +1,100 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import RepositorySearchService from '../../services/repository-search.service';
+import IssueSearchService from '../../services/issue-search.service';
+import * as R from 'ramda';
 
 @Component({
   selector: 'app',
-  template: `<p>Hello World</p>`,
+  template: `
+    <div id="app-container">
+      <p>App</p>
+
+      <search-panel
+        (searchTrigger)="searchRepositories($event)"
+        (clearButtonClick)="searchRepositories('')"
+      ></search-panel>
+
+      <content
+        [repositories]="repositories"
+        [selectedRepo]="selectedRepo"
+        [selectedRepoIssues]="selectedRepoIssues"
+        (repositoryClick)="handleRepositoryClick($event)"
+      ></content>
+    </div>
+  `,
 })
 
-export default class AppComponent {
+export default class AppComponent implements OnInit {
+  public repositories: any[];
+  public selectedRepo: any;
+  public selectedRepoIssues: any[];
 
+  constructor(
+    private issueSearchService: IssueSearchService,
+    private repositorySearchService: RepositorySearchService
+  ) {}
+
+  ngOnInit(): void {
+    this.repositories = [];
+    this.resetSelectedProps();
+
+    // initiate an "empty" search
+    this.searchRepositories('');
+  }
+
+  resetSelectedProps(): void {
+    this.selectedRepo = null;
+    this.selectedRepoIssues = [];
+  }
+
+  searchRepositories(query: string): void {
+    // small hack to allow "empty" search
+    query = query ? query : 'size:>=0';
+
+    const setRepositores = repositories => {
+      this.repositories = repositories;
+    };
+
+    const handleError = err => {
+      // TODO proper error handling
+      console.error(err);
+      setRepositores([]);
+    };
+
+    const resetSelectedProps = () => this.resetSelectedProps();
+
+    this.repositorySearchService.search(query)
+      .subscribe(
+        setRepositores,
+        handleError,
+        resetSelectedProps,
+      );
+  }
+
+  searchIssuesByRepoFullName(repoFullName: string): void {
+    const setSelectedRepoIssues = issues => {
+      this.selectedRepoIssues = issues;
+    };
+
+    const handleError = err => {
+      // TODO proper error handling
+      console.error(err);
+      setSelectedRepoIssues([]);
+    };
+
+    this.issueSearchService.searchByRepoFullName(repoFullName)
+      .subscribe(
+        setSelectedRepoIssues,
+        handleError,
+      );
+  }
+
+  handleRepositoryClick(repositoryId: number): void {
+    // TODO more FP-ish
+    const findRepoByRepositoryId = R.find(repository => repository.id === repositoryId);
+    const repo = findRepoByRepositoryId(this.repositories);
+
+    this.selectedRepo = repo;
+    this.searchIssuesByRepoFullName(this.selectedRepo.fullName);
+  }
 }
