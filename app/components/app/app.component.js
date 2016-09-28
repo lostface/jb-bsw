a2b11050b2a118abb1fe815b6885859fbef70d96
Zoltan Feher <lostface@fastmail.fm>
2016-09-28 14:03:03 +0200

    Implement repo details dialog

--- app/components/app/app.component.js
+++ app/components/app/app.component.js
@@ -1,11 +1,13 @@
+import angular from 'angular';
 import R from 'ramda';
 import template from './app.component.html';
 
 class AppController {
-  constructor(repositorySearchService, issueSearchService) {
+  constructor(repositorySearchService, issueSearchService, $mdDialog) {
     'ngInject';
     this.repositorySearchService = repositorySearchService;
     this.issueSearchService = issueSearchService;
+    this.$mdDialog = $mdDialog;
   }
 
   $onInit() {
@@ -66,6 +68,45 @@ class AppController {
 
     this.selectedRepo = repo;
     this.searchIssuesByRepoFullName(this.selectedRepo.fullName);
+
+    this.showRepositoryDetailsDialog();
+  }
+
+  showRepositoryDetailsDialog() {
+    const { $mdDialog } = this;
+
+    $mdDialog.show({
+      template: `
+        <md-dialog>
+          <md-toolbar>
+            <div class="md-toolbar-tools">
+              <h2>{{::$ctrl.repository.fullName}} Details</h2>
+            </div>
+          </md-toolbar>
+
+          <md-dialog-content style="margin-left: 16px">
+            <issue-list issues="$ctrl.repositoryIssues"></issue-list>
+          </md-dialog-content>
+
+          <md-dialog-actions layout="row">
+            <md-button ng-click="$ctrl.hide()">Close</md-button>
+          </md-dialog-actions>
+        </md-dialog>
+      `,
+      parent: angular.element(document.body),
+      clickOutsideToClose: true,
+      escapeToClose: true,
+      controller: () => {},
+      controllerAs: '$ctrl',
+      // TODO resolve can be used here so the dialog won't open until the promise
+      //  is resolved
+      locals: {
+        repository: this.selectedRepo,
+        repositoryIssues: this.selectedRepoIssues,
+        hide: () => { $mdDialog.hide(); },
+      },
+      bindToController: true,
+    });
   }
 }
 
